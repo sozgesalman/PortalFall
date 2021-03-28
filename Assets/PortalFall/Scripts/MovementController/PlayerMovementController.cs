d15160267306980796f9f1d81b77a6f1bf65d66c
Ozge Salman <sozgesalman@gmail.com>
2021-03-28 22:58:35 +0300

    +refactor: PlayerMovementControl and PlayerMovementSettings scripts created

---
+++ Assets/PortalFall/Scripts/MovementController/PlayerMovementController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PortalFall.PlayerControls
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [SerializeField] GameObject _player;
+        [SerializeField] private Transform _transform;
+        [SerializeField] private bool _canFall;
+        [SerializeField] private PlayerMovementSettings _playerMovementSettings;
+       
+        
+        void Start()
+        {
+            Invoke("DisplayPlayer", 2);
+            StartCoroutine(ReadyToFall());
+            
+        }
+        public void DisplayPlayer()
+        {
+            _player.SetActive(true);
+        }
+
+       
+        void Update()
+        {
+            _transform.Translate(Vector3.down * _playerMovementSettings.FallSpeed * Time.deltaTime, Space.World);
+        }
+
+        IEnumerator ReadyToFall()
+        {
+            _canFall = false;
+            yield return new WaitForSeconds(_playerMovementSettings.TimeBeforeFall);
+            _canFall = true;
+        }
+
+        
+    }
+}
---
+++ Assets/PortalFall/Scripts/MovementController/PlayerMovementSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PortalFall.PlayerControls
+{
+    [CreateAssetMenu(menuName = "Portal Fall/Player/Movement Settings")]
+
+    public class PlayerMovementSettings : ScriptableObject
+    {
+        public float FallSpeed = 5;
+        public float PortalSpeed = 2;
+        public float TimeBeforeFall = 5;
+
+
+        
+    }
+}
