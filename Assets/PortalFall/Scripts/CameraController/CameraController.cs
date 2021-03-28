11d3a5a6532f71ceaab8c43504ea00d60df6f7bd
Ozge Salman <sozgesalman@gmail.com>
2021-03-29 02:50:14 +0300

    +refactor: CameraController and CameraSettings created

---
+++ Assets/PortalFall/Scripts/CameraController/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PortalFall.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CameraSettings _cameraSettings;
+        [SerializeField] private Transform _positionTarget;
+        [SerializeField] private Transform _cameraTransform;
+        
+
+        private void Update()
+        {
+            CameraMovementFollow();            
+        }
+
+
+        private void CameraMovementFollow()
+        {
+            Vector3 offset = (_cameraTransform.right * _cameraSettings.PositionOffSet.x) + (_cameraTransform.up * _cameraSettings.PositionOffSet.y) +
+                (_cameraTransform.forward * _cameraSettings.PositionOffSet.z);
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _positionTarget.position + offset,
+                Time.deltaTime * _cameraSettings.PositionLerb);
+
+            
+        }
+    }
+}
---
+++ Assets/PortalFall/Scripts/CameraController/CameraSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PortalFall.Camera
+{
+    [CreateAssetMenu (menuName ="Portal Fall/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        
+        [Header("Position")]
+        [SerializeField] private Vector3 _positionOffSet;
+        public Vector3 PositionOffSet { get { return _positionOffSet; } }
+
+        [SerializeField] private float _positionLerp = 1;
+        public float PositionLerb { get { return _positionLerp; } }
+    }
+}
