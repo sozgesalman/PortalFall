be9ddaa3b82907983173f3ac535c8458492d4801
Ozge Salman <sozgesalman@gmail.com>
2021-04-04 02:27:22 +0300

    +refactor:Collision settings

--- Assets/PortalFall/Scripts/MovementController/ClosePortalController.cs
+++ Assets/PortalFall/Scripts/MovementController/ClosePortalController.cs
@@ -15,7 +15,8 @@ namespace PortalFall.Portal
         [SerializeField] private Transform _transform ;
         [SerializeField] private Transform _player;
         [SerializeField] private PlayerMovementSettings _playerMovementSettings ;
-        [SerializeField] private GameObject[] _forVisibilty ;        
+        [SerializeField] private GameObject[] _forVisibilty ;
+        private Collision _collision;
 
         void Update()
         {
@@ -56,8 +57,34 @@ namespace PortalFall.Portal
                 }
                 _transform.position = Vector3.MoveTowards(_transform.position, waypoints[current].transform.position, Time.deltaTime * _playerMovementSettings.PortalSpeed) ;
             }
-            
+            else if (Input.GetKeyUp(KeyCode.V))
+            {
+                for (int i = 0; i < _forVisibilty.Length; i++)
+                {
+                    _forVisibilty[i].GetComponent<Renderer>().enabled = true;
+                    _forVisibilty[i].GetComponent<Collider>().enabled = true;
+                }
+                
+                //_transform.GetComponent<Transform>().enabled = false;
+            }
 
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            //Check for a match with the specified name on any GameObject that collides with your GameObject
+            if (collision.gameObject == _player)
+            {
+                //If the GameObject's name matches the one you suggest, output this message in the console
+                Debug.Log("Do something here");
+            }
+
+            //Check for a match with the specific tag on any GameObject that collides with your GameObject
+            if (collision.gameObject == _line)
+            {
+                //If the GameObject has the same tag as specified, output this message in the console
+                Debug.Log("Do something else here");
+            }
         }
     }
 }
